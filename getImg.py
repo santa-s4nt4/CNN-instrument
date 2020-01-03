eaa381c915dc91daa45c28554ba61ef491ab1886
santa-sukitoku <santanaruse@gmail.com>
2020-01-03 21:24:55 +0900

    first commit

---
+++ getImg.py
@@ -0,0 +1,7 @@
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+arguments = {"keywords": "drum", "limit": 10000,
+             "chromedriver": "C:\\Users\\santa\\chromedriver_win32\\chromedriver.exe", "print_urls": True}
+response.download(arguments)
