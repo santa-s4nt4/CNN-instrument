f96480365b4a5a0f8071bd302e7f19cc888b443a
santa-sukitoku <santanaruse@gmail.com>
2020-01-05 00:23:28 +0900

    画像の前処理部分の実装を完了

--- cnn_model.py
+++ cnn_model.py
@@ -20,3 +20,28 @@ from keras.utils import np_utils
 FileNames = ["img1.npy", "img2.npy", "img3.npy"]
 ClassNames = ["drum", "guiter", "piano"]
 hw = {"height": 16, "weight": 16}
+directory = "/dataset/reshaped/"
+
+
+# 前処理
+def PreProcess(dirname, filename, var_amount=3):
+    num = 0  # 画像ファイルのカウント
+    arrlist = []  # 画像ファイルをnumpy型に変換したものを入れるリスト
+    files = glob.glob(directory + "*.jpg")
+
+    # 画像処理
+    for imgfile in files:
+        img = load_img(imgfile, target_size=(
+            hw["height"], hw["weight"]))  # 画像ファイルの読み込み
+        array = img_to_array(img) / 255  # numpy型データをリストに追加
+        arrlist.append(array)
+        for i in range(var_amount - 1):
+            arr2 = array
+            arr2 = random_rotation(arr2, rg=360)
+            arrlist.append(arr2)  # numpy型データをリストに追加
+        num += 1
+
+    # 保存
+    nplist = np.array(arrlist)
+    np.save(filename, nplist)
+    print(">> " + directory + "から" + str(num) + "個のファイルの読み込み成功")
