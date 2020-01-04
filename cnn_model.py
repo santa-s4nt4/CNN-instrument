ae19cba5b33b6658c262d2e6617eba5d8223891e
santa-sukitoku <santanaruse@gmail.com>
2020-01-04 23:40:43 +0900

    処理共通のパラメータを追加

--- cnn_model.py
+++ cnn_model.py
@@ -15,3 +15,8 @@ from keras.callbacks import LearningRateScheduler
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 from keras.utils import np_utils
+
+# 処理共通のパラメータ
+FileNames = ["img1.npy", "img2.npy", "img3.npy"]
+ClassNames = ["drum", "guiter", "piano"]
+hw = {"height": 16, "weight": 16}
