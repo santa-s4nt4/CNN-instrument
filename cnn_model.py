b03bf5917920ac3b51cc4fa957a4e35a6ce27dc1
santa-sukitoku <santanaruse@gmail.com>
2020-01-11 16:35:39 +0900

    typo fix

--- cnn_model.py
+++ cnn_model.py
@@ -19,20 +19,19 @@ from keras.utils import np_utils
 # 処理共通のパラメータ
 FileNames = ["img1.npy", "img2.npy", "img3.npy"]
 ClassNames = ["drum", "guiter", "piano"]
-hw = {"height": 16, "weight": 16}
-directory = "/dataset/reshaped/"
+hw = {"height": 32, "width": 32}
 
 
 # 前処理
 def PreProcess(dirname, filename, var_amount=3):
     num = 0  # 画像ファイルのカウント
     arrlist = []  # 画像ファイルをnumpy型に変換したものを入れるリスト
-    files = glob.glob(directory + "*.jpg")
+    files = glob.glob(dirname + "/*.jpg")
 
     # 画像処理
     for imgfile in files:
         img = load_img(imgfile, target_size=(
-            hw["height"], hw["weight"]))  # 画像ファイルの読み込み
+            hw["height"], hw["width"]))    # 画像ファイルの読み込み
         array = img_to_array(img) / 255  # numpy型データをリストに追加
         arrlist.append(array)
         for i in range(var_amount - 1):
@@ -44,7 +43,7 @@ def PreProcess(dirname, filename, var_amount=3):
     # 保存
     nplist = np.array(arrlist)
     np.save(filename, nplist)
-    print(">> " + directory + "から" + str(num) + "個のファイルの読み込み成功")
+    print(">> " + dirname + "から" + str(num) + "個のファイルの読み込み成功")
 
 
 # モデル構築
